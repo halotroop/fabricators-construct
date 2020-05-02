2abfadb2f4e1f21b6bdf6ee9a02210d66ab48f5f
Devan-Kerman <devan@cleverpath.com>
2020-05-02 16:51:35 -0500

    wat

--- src/main/java/com/halotroop/tconstruct/registry/ItemRegistry.java
+++ src/main/java/com/halotroop/tconstruct/registry/ItemRegistry.java
@@ -6,11 +6,11 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ItemRegistry {
-	
+
 	public static void registerAll() {
-	
+
 	}
-	
+
 	public static Item registerItem(String name, Item item) {
 		System.out.println("Registering " + name + " as a(m) " + item.getClass().getSimpleName());
 		return Registry.register(Registry.ITEM, new Identifier(TConstruct.modid, name.toLowerCase()), item);
